74f6e290d8de20b672cf53d30dd268573b114c57
ashish-3216 <ashish.work.3216@gmail.com>
2025-03-26 14:32:47 +0530

    decline/approve appointment

--- api/v1/contollers/appointmentController.js
+++ api/v1/contollers/appointmentController.js
@@ -2,17 +2,20 @@ import {
   submitAppointment,
   approveAppointment,
   updateSameTimeAppointments,
+  declineAppointment,
+  getDetails
 } from "../services/appointmentService.js";
 import express from "express";
 import { authenticateUser } from "../middleware/middleware.js";
 const router = express.Router();
-
-router.post("/", authenticateUser, async (req, res) => {
+//
+router.post("/",  authenticateUser , async (req, res) => {
   try {
     const user_email = req.user_email; // Access from middleware
-    const { doctor_id, appointment_time, location, type } = req.body;
+    const { doctor_id, appointment_time, appointment_date ,location, type } = req.body;
 
     const result = await submitAppointment({
+      appointment_date,
       user_email,
       doctor_id,
       appointment_time,
@@ -36,23 +39,28 @@ router.post("/", authenticateUser, async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/approve", async (req, res) => {
   try {
-    const { id, appointment_time, location, type } = req.body;
+    const { id, appointment_time ,appointment_date,doctor_id} = req.body;
     const result = await approveAppointment(id);
     if (result.success) {
-      const response = await updateSameTimeAppointments(appointment_time);
+      const response = await updateSameTimeAppointments(appointment_date,appointment_time,doctor_id);
       if (response.success) {
         return res.status(200).json({
           success: true,
           message: "Appointment approved successfully",
         });
       } else {
-        console.error("Failed to approve appointment:", result.message);
+        console.error("Failed to approve appointment:", response.message);
+        return res
+          .status(400)
+          .json({ success: false, message: response.message });
+      }
+    }else{
+      console.error("Failed to approve appointment:", result.message);
         return res
           .status(400)
           .json({ success: false, message: result.message });
-      }
     }
   } catch (err) {
     console.error("Error in API controller:", err.message);
@@ -61,4 +69,38 @@ router.post("/", async (req, res) => {
       .json({ success: false, message: "Internal Server Error" });
   }
 });
+
+router.post('/decline',async (req,res)=>{
+  try{
+    const {id} = req.body ;
+  const result = await declineAppointment(id) ;
+  if(result.success){
+    res.status(200).json({success : true , message : "Appointment Declined"});
+  }else{
+    res.status(400).json({success : false , message : 'Appointment not declined'});
+  }
+  }catch(err){
+    console.error("Error in API controller:", err.message);
+    res.status(500).json({success : false , message : 'Appointment not declined || internal server error'});
+  }
+  
+})
+
+router.get('/details',async (req,res)=>{
+  try{
+    const result = await getDetails(req.body) ;
+    if(result.success){
+      res.status(200).json(
+        {
+          success : true,
+          data : result.data
+        }
+      )
+    }else{
+      throw new Error('error in get api')
+    }
+  }catch(err){
+    return res.status(500).json({message : err.message});
+  }
+})
 export default router;
--- api/v1/services/appointmentService.js
+++ api/v1/services/appointmentService.js
@@ -2,13 +2,13 @@ import express from "express";
 import pool from "../../db/config.js";
 export const submitAppointment = async (data) => {
   try {
-    const { user_email, doctor_id, appointment_time, location, type } = data;
+    const { user_email, doctor_id, appointment_time, location, type ,appointment_date } = data;
     const status = "Pending"; // Default status for approval
 
     const check = await pool.query(
-      `INSERT INTO appointments (user_email, doctor_id, appointment_date, location, type, status) 
-         VALUES ($1, $2, $3, $4, $5, $6)`,
-      [user_email, doctor_id, appointment_time, location, type, status]
+      `INSERT INTO appointments (user_email, doctor_id, appointment_time, location, type, status , appointment_date) 
+         VALUES ($1, $2, $3, $4, $5, $6 , $7)`,
+      [user_email, doctor_id, appointment_time, location, type, status , appointment_date]
     );
 
     console.log("Appointment booked");
@@ -50,13 +50,43 @@ export const approveAppointment = async (id) => {
     };
   }
 };
-
-export const updateSameTimeAppointments = async (date) => {
-  try {
+export const declineAppointment = async (id) => {
+  try{
     const result = await pool.query(
       `UPDATE appointments SET status = $1 
-       WHERE appointment_date = $2 AND status = $3`,
-      ["declined", date, "Pending"]
+       WHERE id = $2`,
+      ["Pending",id]
+    );
+    if(result.rowCount === 0){
+      return{
+        success : false ,
+        message: "Appointment not found or already approved.",
+      }
+    }
+    console.log('Appointment Declined') ;
+    return{
+      success : true ,
+      message : 'Appointment declined successfully'
+    }
+  }catch(err){
+    console.log('Error in Controller api');
+    return{
+      success : false,
+      message : 'Error in Controller api'
+    }
+  }
+}
+export const updateSameTimeAppointments = async (date,time,doctor_id) => {
+  try {
+    const result = await pool.query(
+      `UPDATE appointments 
+       SET status = $1 
+       WHERE appointment_date = $2 
+         AND appointment_time = $3 
+         AND status = $4 
+         AND doctor_id = $5
+       RETURNING *`,
+      ["declined", date, time, "Pending",doctor_id]
     );
 
     if (result.rowCount === 0) {
@@ -80,3 +110,57 @@ export const updateSameTimeAppointments = async (date) => {
     };
   }
 };
+// export const updateSameTimeAppointments = async ( date, time , doc_id) => {
+//   try {
+//     console.log("Updating with:", { date, time });
+
+//     const result = await pool.query(
+//       `UPDATE appointments 
+//        SET status = $1 
+//        WHERE appointment_date = $2 
+//          AND appointment_time = $3 
+//          AND status = $4
+//          AND doctor_id = $5
+//        RETURNING *`,
+//       ["declined", date, time, "Pending", doc_id]
+//     );
+//     console.log(`UPDATE appointments SET status = 'declined' WHERE appointment_date = '${date}' AND appointment_time = '${time}' AND status = 'Pending' AND doctor_id = '${doc_id}'`);
+//     if (result.rowCount === 0) {
+//       console.log("No pending appointments found on the given date and time.");
+//       return {
+//         success: false,
+//         message: "No pending appointments found for the given date and time.",
+//       };
+//     }
+
+//     console.log(`${result.rowCount} appointment(s) updated to declined.`);
+//     return {
+//       success: true,
+//       message: `${result.rowCount} appointment(s) updated to declined.`,
+//     };
+//   } catch (err) {
+//     console.error("Database Error:", err);
+//     return {
+//       success: false,
+//       message: "Can't update appointments right now.",
+//     };
+//   }
+// };
+export const getDetails = async ({ doc_id , date })=>{
+  try{
+    const result = await pool.query(
+      `SELECT * FROM APPOINTMENTS where doctor_id = $1 AND appointment_date = $2`,
+      [doc_id,date]
+    );
+    return {
+      success: true,
+      data: result.rows,
+    };
+  }catch(err){
+    console.log('db error')
+    return{
+      success : false ,
+      message : 'can find or DataBase error' 
+    }
+  }
+}
