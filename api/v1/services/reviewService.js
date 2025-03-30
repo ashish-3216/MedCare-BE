13060d46970726a8cffb4deef4201d6a094d9b97
ashish-3216 <ashish.work.3216@gmail.com>
2025-03-30 12:52:54 +0530

    project complete

--- api/v1/services/reviewService.js
+++ api/v1/services/reviewService.js
@@ -3,7 +3,7 @@ import pool from "../../db/config.js";
 export const getReviews = async () => {
   try {
     const result = await pool.query(
-      `SELECT a.* , b.doc_name , b.img_url , b.specialization 
+      `SELECT a.* , b.doc_name , b.specialization 
             FROM REVIEWS as a
             JOIN doctors as b
             ON a.doc_id = b.id `,
@@ -29,10 +29,10 @@ export const getReviews = async () => {
 
 export const addReview = async (data) => {
   try {
-    const { doctor_id, review , rating } = data;
+    const { doctor_id, review , rating , name } = data;
     const result = await pool.query(
-      `INSERT INTO reviews (doc_id, review_text,rating) VALUES ($1, $2,$3) RETURNING *`,
-      [doctor_id, review,rating]
+      `INSERT INTO reviews (doc_id, review_text,rating,name) VALUES ($1, $2,$3,$4) RETURNING *`,
+      [doctor_id, review,rating,name]
     );
     console.log("Review inserted:", result.rows[0]);
     return{
