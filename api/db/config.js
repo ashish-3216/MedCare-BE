8592c6596d27aa38191e5e023d99a87a6e92d892
ashish-3216 <ashish.work.3216@gmail.com>
2025-04-23 20:51:55 +0530

    deploying

--- api/db/config.js
+++ api/db/config.js
@@ -5,20 +5,14 @@ import util from 'util'
 dotenv.config() ;
 const {Pool} = pkg ;
 
+
 const sql_pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // ✅ needed for many hosted services
-  }
+    ssl: {
+      rejectUnauthorized: false // ✅ needed for many hosted services
+    }
+  });
 
-    // user : process.env.user,
-    // database : process.env.database,
-    // password : process.env.dbPassword,
-    // port : process.env.dbPort,
-    // max : process.env.max ,
-    // idleTimeoutMillis : process.env.idleTimeoutMillis ,
-    // connectionTimeoutMillis : process.env.connectionTimeoutMillis,
-});
 
 const pool = {
     query : (sql,args) =>{
