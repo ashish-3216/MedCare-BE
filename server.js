22ec2951ea58e6c0731971438ac542e3d4e76e23
ashish-3216 <ashish.work.3216@gmail.com>
2025-05-22 15:49:07 +0530

    admin acesss donw

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://med-care-eta.vercel.app , https://med-care-admin-portal.vercel.app "] ,
+  origin: ["http://localhost:3000","https://med-care-eta.vercel.app" , "https://med-care-admin-portal.vercel.app"] ,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type",
